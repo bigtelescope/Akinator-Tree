40596aa2a3574e7fc0d6307eca1fcf21781ec438
bigtelescope <jonnyuz99@gmail.com>
2018-12-02 23:15:12 +0300

    fuck

--- tree.h
+++ tree.h
@@ -1,31 +1,44 @@
 #ifndef TREE_H_
 #define TREE_H_
 
-
 typedef struct node
 {
-	int value;
-	node * left;
-	node * right;
-	node * prev;
+	char * str;
+	struct node * left;
+	struct node * right;
+	struct node * prev;
 }Node;
-
-typedef struct root
-{
-	int value;
-	node * left;
-	node * right;
-}Root;
 
 typedef struct tree
 {
-	Root * root;
+	Node * root;
 	int amount;
 }Tree;
 
+//Creates tree
 Tree * CreateTree();
-void TreeDestroy(Tree * point);
-Root * CreateRoot(int value);
-Node * CreateNode(int value);
+
+//Function for cleaning a memory
+void TreeDestroy(Tree *);
+
+//Creates node with certain value
+Node * CreateNode(char * value);
+
+//Recursive function for bypassing of a tree for PrintGraph
+int CyclePrint(FILE *, Node *);
+
+//Function for making file.dot
+int PrintGraph(Tree *);
+
+//Function that writes a structure of a tree
+int WriteGraph(Tree *);
+
+//Recursive function for bypassing of a tree for WriteGraph
+int CycleWrite(FILE *, Node *);
+
+long long FSize(FILE * ptrfile);
+
+//Node * ReadGraph(FILE *, char *, char*);
+Node * ReadGraph(FILE *);
 
 #endif
